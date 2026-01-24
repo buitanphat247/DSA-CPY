7aaffdb0ad7501adceb2cc537d202c432b22c509
buitanphat247 <tanp28062@gmail.com>
2026-01-25 01:16:54 +0700

    Rename Sum(x^n/n!) to Sum(x^n/Sum06

--- Sum(x^n/Sum06/main.py
+++ Sum(x^n/Sum06/main.py
@@ -1,10 +1,9 @@
-n = float(input())
-x = float(input())
+n, x = map(int, input().split())
 
 result = 1.0
 sum = 1.0
 for i in range(1, n + 1):
-    result = result * x / i
+    result = result * x / float(i)
     sum += result
 
 print(f"{sum:.2f}")
