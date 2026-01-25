4e91641137f0e8724bd77013e7212ab87c04e9ea
buitanphat247 <tanp28062@gmail.com>
2026-01-25 10:40:55 +0700

    Add KTLT_016 implementation, update LCS and Enviroment_Cpp

--- Enviroment_Cpp/main/main.cpp
+++ Enviroment_Cpp/main/main.cpp
@@ -12,22 +12,21 @@ int main() {
     READ("1");
   ll n;
   cin >> n;
-  vector<ll> l(n + 10, 0);
-  vector<ll> h(n + 10, 0);
-  for (ll i = 1; i <= n; i++)
-    cin >> l[i];
-  for (ll i = 1; i <= n; i++)
-    cin >> h[i];
-  vector<vector<ll>> dp(n + 10, vector<ll>(3, 0));
-  dp[1][0] = 0;
-  dp[1][1] = l[1];
-  dp[1][2] = 0;
+  vector<ll> x(n);
+  for (ll i = 0; i < n; i++)
+    cin >> x[i];
+  vector<ll> dp;
+  for (ll i = 0; i < n; i++) {
+    auto it = lower_bound(dp.begin(), dp.end(), x[i]);
 
-  for (ll i = 2; i <= n; i++) {
-    dp[i][0] = max({dp[i - 1][0], dp[i - 1][1], dp[i - 1][2]});
-    dp[i][1] = max({dp[i - 1][0], dp[i - 1][1], dp[i - 1][2]}) + l[i];
-    dp[i][2] = dp[i - 1][0] + h[i];
+    if (it == dp.end()) {
+      dp.push_back(x[i]);
+    } else {
+      *it = x[i];
+    }
   }
-
-  cout << max({dp[n][0], dp[n][1], dp[n][2]}) << endl;
+for(auto it:dp)
+    cout<<it<<" ";
+cout<<endl;
+  cout << dp.size() << endl;
 }
